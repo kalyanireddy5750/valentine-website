86399f9645d82e483f32d25598e80cafa8e2dc95
kalyanireddy5750 <kalyanireddy.5750@gmail.com>
2026-02-02 15:42:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const message = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 250;
+  const y = Math.random() * 80;
+
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+yesBtn.addEventListener("click", () => {
+  message.innerHTML = "YAYYYY!! 💖🥰 You just made my day 💕";
+});
